35cc9c1d7dbc7bb1e89497dba487e04278676e81
echiyembekeza <username@users.noreply.github.com>
2019-10-11 23:04:48 -0400

    update

--- sprint-challenge/aq_dashboard/app.py
+++ sprint-challenge/aq_dashboard/app.py
@@ -1,9 +1,10 @@
 """OpenAQ Air Quality Dashboard with Flask."""
-from flask import Flask, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from decouple import config
 from os import getenv
 import openaq
+import requests
 #import DB
 
 APP = Flask(__name__)
